4e28f7877e8b3c6196374de5bff1354cc9198ddf
Sameeh <asp.net.sameeh.hamdan@gmail.com>
2022-07-29 15:25:16 +0300

    Add Practice01

---
+++ Practice/Practice1/Practice1.Web/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice1.Web.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetName()
+        {
+            return Ok("Sameeh Abutaima");
+        }
+    }
+}
