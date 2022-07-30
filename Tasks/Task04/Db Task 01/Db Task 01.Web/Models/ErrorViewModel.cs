815aecbe80b60ac1a8abce6e24bc089db84f82d3
Sameeh <asp.net.sameeh.hamdan@gmail.com>
2022-07-30 13:23:30 +0300

    Add Task04

---
+++ Tasks/Task04/Db Task 01/Db Task 01.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Db_Task_01.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
